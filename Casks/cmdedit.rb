b7069995b934cf8d22349b1081f795783c03f3d6
Zijie Cai <leo20020208@gmail.com>
2026-03-25 17:11:12 -0400

    Update CmdEdit cask checksum for v1.0.0 notarized release

--- Casks/cmdedit.rb
+++ Casks/cmdedit.rb
@@ -1,6 +1,6 @@
 cask "cmdedit" do
   version "1.0.0"
-  sha256 "a815edf57cf65dbc29a67ccdfc0547690b4fc8feb47ff34828dd66bfe1b9a6b2"
+  sha256 "42fc6d1669e4583cf5d307c968111c16c53b194a31eba54592ee14409b21e8ba"
 
   url "https://github.com/zijie-cai/CmdEdit/releases/download/v1.0.0/CmdEdit-1.0.0.zip"
   name "CmdEdit"
